bd6a772f0b09872c3fee282ad178c8f6382e7eff
Ryan Hall <ryanhallcs@gmail.com>
2016-09-18 15:53:07 -0500

    removing PizzaState in lieu of just a single high level entry point

--- src/js/pages/Main.js
+++ src/js/pages/Main.js
@@ -2,18 +2,48 @@ import React from 'react';
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import PizzaState from "./PizzaState";
+
+import ResourceDisplay from "../sections/ResourceDisplay";
+import EventDisplay from "../sections/EventDisplay";
+import MapDisplay from "../sections/MapDisplay";
+import { InteractionDisplay } from "../sections/InteractionDisplay";
+import { Row, Col } from 'react-bootstrap';
+
+import ResourceStore from "../stores/ResourceStore";
+import TriggerStore from "../stores/TriggerStore";
+import DisplayActions from "../actions/DisplayActions";
 
 import { Grid } from 'react-bootstrap';
 
-export default class Main extends React.Component {
-  render () {
+export default React.createClass({
+  getInitialState: function() {
+      DisplayActions.pushDisplay('home');
+      setInterval(ResourceStore.tick.bind(ResourceStore), 100); // updates per tenth second
+      setInterval(TriggerStore.tick.bind(TriggerStore), 500);
+      return {};
+  },
+  render: function() {
     return (
       <Grid bsClass={'fluid fill'}>
         <Header />
-        <PizzaState />
+        <Row className='full-height'>
+              <Col md={3}>
+                  <ResourceDisplay />
+              </Col>
+              <Col md={4}>
+                  <InteractionDisplay />
+              </Col>
+              <Col md={5}>
+                  <Row className='interaction main-layout-border'>
+                      <Col md={12}>
+                          <EventDisplay />
+                      </Col>
+                  </Row>
+                  <MapDisplay />
+              </Col>
+          </Row>
         <Footer />
       </Grid>
     );
   }
-}
+});
